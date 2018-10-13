14affe366352cc313dfac9d75fdc34b7b8a2d1e3
Angelica  Ramirez <angelicaramirez@Angelicas-MacBook-Pro.local>
2018-10-13 14:11:02 -0700

    Selects Account for Google Drive

--- app/src/main/java/com/musiquera/bsanchezg/musiquera/mainActivity/MainActivity.java
+++ app/src/main/java/com/musiquera/bsanchezg/musiquera/mainActivity/MainActivity.java
@@ -1,15 +1,82 @@
 package com.musiquera.bsanchezg.musiquera.mainActivity;
 
+import android.content.IntentSender;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.drive.Drive;
 import com.musiquera.bsanchezg.musiquera.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener {
+
+    private GoogleApiClient mGoogleApiClient;
+
+    private Button selectAccount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        selectAccount = findViewById(R.id.bs__select_account);
+
+        selectAccount.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        selectAccount();
+                    }
+                }
+        );
+    }
+
+    private void selectAccount() {
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addApi(Drive.API)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .build();
+        }
+
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        Toast.makeText(getApplicationContext(), "connected", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+        if (!connectionResult.hasResolution()) {
+
+            // show the localized error dialog.
+            GoogleApiAvailability.getInstance().getErrorDialog(this, connectionResult.getErrorCode(), 0).show();
+            return;
+        }
+
+        try {
+
+            connectionResult.startResolutionForResult(this, 0);
+
+        } catch (IntentSender.SendIntentException e) {
+
+        }
     }
 }
